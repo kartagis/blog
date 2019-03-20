f8ae886f755c7e3d52a44bd832cab47e858532a6
Kartagis <tolga@ozses.net>
2019-03-20 16:20:33 +0300

    database changed

--- database/migrations/2019_03_20_130530_create_tasks_table.php
+++ database/migrations/2019_03_20_130530_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBlogTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateBlogTable extends Migration
      */
     public function up()
     {
-        Schema::create('blog', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id');
-            $table->text('title');
-            $table->longText('body');
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateBlogTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blog');
+        Schema::dropIfExists('tasks');
     }
 }
